9b7a5ec192d0d6e7b83f5a5ec0bcad54c2d9ba12
Maverick Sison <mavericksison@outlook.com>
2022-03-25 11:23:52 -0700

    first commit

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html", phrase="hello", times=5)	# notice the 2 new named arguments (phrase, time!)
+
+if __name__=="__main__":
+    app.run(debug=True)
+
